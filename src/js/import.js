f2a8bb05bdd12b0f6d145e5347a99ddac46db68c
intfrog <intfrog@gmail.com>
2019-03-15 17:13:47 -0400

    src and prototype

---
+++ src/js/import.js
@@ -0,0 +1,4 @@
+document.write('<script src="lib/jquery-3.3.1.min.js"></script>');
+document.write('<script src="lib/semantic/semantic.min.js"></script>');
+document.write('<link rel="stylesheet" href="lib/semantic/semantic.min.css"/>');
+// document.write('<script src="lib/markdown-it.min.js"></script>');
